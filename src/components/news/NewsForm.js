9433061f3b9fcf767400fe290659dd8b7d771b7c
jamesnitz <jamesanitz2@gmail.com>
2020-01-30 11:37:05 -0600

    successfully posting new news to db

--- src/components/news/NewsForm.js
+++ src/components/news/NewsForm.js
@@ -1,92 +1,112 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext, useState, useEffect, useRef } from "react"
 import { NewsContext } from "./NewsProvider"
 import { UserContext } from "../users/UserProvider"
 
 
 export default props => {
-    const { users } = useContext(UserContext)
-    const { addNews, news, editNews } = useContext(NewsContext)
-    const [singleNews, setSingleNews] = useState({})
+  const { users } = useContext(UserContext)
+  const { addNews, news, editNews } = useContext(NewsContext)
+  const [singleNews, setSingleNews] = useState("")
+  const newsTitle = useRef("")
+  const newsUrl = useRef("")
+  const newsSynopsis = useRef("")
+  const editMode = props.match.params.hasOwnProperty("newsId")
 
-    const editMode = props.match.params.hasOwnProperty("newsId")
+  const handleControlledInputChange = (event) => {
+    /*
+        When changing a state object or array, always create a new one
+        and change state instead of modifying current one
+    */
+    const newSingleNews = Object.assign({}, singleNews)
+    newSingleNews[event.target.name] = event.target.value
+    setSingleNews(newSingleNews)
+  }
 
-    const handleControlledInputChange = (event) => {
-        /*
-            When changing a state object or array, always create a new one
-            and change state instead of modifying current one
-        */
-        const newSingleNews = Object.assign({}, singleNews)
-        newSingleNews[event.target.name] = event.target.value
-        setSingleNews(newSingleNews)
+  const setDefaults = () => {
+    if (editMode) {
+      const newsId = parseInt(props.match.params.newsId)
+      const selectedNews = news.find(n => n.id === newsId) || {}
+      setSingleNews(selectedNews)
+    }
+  }
+
+  useEffect(() => {
+    setDefaults()
+  }, [news])
+
+  const constructNewNews = () => {
+    if (editMode) {
+        editNews({
+            id: news.id,
+            title: newsTitle.current.value,
+            synopsis: newsSynopsis.current.value,
+            date: news.date,
+            userId: parseInt(localStorage.getItem("nutshell_user"))
+        })
+            .then(() => props.history.push("/"))
+    } else {
+      const date = new Date();
+      const currentDate = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) +
+      '-' + date.getDate().toString().padStart(2, 0);
+        addNews({
+            title: newsTitle.current.value,
+            synopsis: newsSynopsis.current.value,
+            url: newsUrl.current.value,
+            date: new Date(currentDate).toLocaleDateString('en-US'),
+            userId: parseInt(localStorage.getItem("nutshell_user"))
+        })
+            .then(() => props.history.push("/"))
     }
 
-    const setDefaults = () => {
-        if (editMode) {
-            const newsId = parseInt(props.match.params.newsId)
-            const selectedNews = news.find(n => n.id === newsId) || {}
-            setSingleNews(selectedNews)
-        }
-    }
+  }
 
-    useEffect(() => {
-        setDefaults()
-    }, [news])
-
-    const constructNewNews = () => {
-            // if (editMode) {
-            //     editNews({
-            //         id: news.id,
-            //         title: animal.name,
-            //         synopsis: animal.breed,
-            //         date: locationId,
-            //         userId: parseInt(localStorage.getItem("nutshell_user"))
-            //     })
-            //         .then(() => props.history.push("/"))
-            // } else {
-            //     addNews({
-            //         title: animal.name,
-            //         synopsis: animal.breed,
-            //         date: locationId,
-            //         userId: parseInt(localStorage.getItem("nutshell_user"))
-            //     })
-            //         .then(() => props.history.push("/"))
-            // }
-        
-    }
-
-    return (
-        <form className="newsForm">
-            <h2 className="newsForm__title">{editMode ? "edit Title" : "Give your news a title"}</h2>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="name">news Title: </label>
-                    <input type="text" name="title" required autoFocus className="form-control"
-                        proptype="varchar"
-                        placeholder="title"
-                        defaultValue={news.title}
-                        onChange={handleControlledInputChange}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="name">news Url: </label>
-                    <input type="text" name="url" required autoFocus className="form-control"
-                        proptype="varchar"
-                        placeholder="url"
-                        defaultValue={news.url}
-                        onChange={handleControlledInputChange}
-                    />
-                </div>
-
-            </fieldset>
-            <button type="submit"
-                onClick={evt => {
-                    evt.preventDefault()
-                    constructNewNews()
-                }}
-                className="btn btn-primary">
-                {editMode ? "Save those changes" : "Add that News!"}
-            </button>
-        </form>
-    )
+  return (
+    <form className="newsForm">
+      <h2 className="newsForm__title">{editMode ? "Slowly Edit" : "Add some sloth news"}</h2>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">news Title: </label>
+          <input type="text" name="title" required autoFocus className="form-control"
+            ref= {newsTitle}
+            proptype="varchar"
+            placeholder="title"
+            defaultValue={news.title}
+            onChange={handleControlledInputChange}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">news Url: </label>
+          <input type="text" name="url" required autoFocus className="form-control"
+            ref= {newsUrl}
+            proptype="varchar"
+            placeholder="url"
+            defaultValue={news.url}
+            onChange={handleControlledInputChange}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="treatment">Synopsis: </label>
+          <textarea type="text" name="synopsis" className="form-control"
+            ref= {newsSynopsis}
+            proptype="varchar"
+            placeholder="What's slothing?"
+            value={news.synopsis}
+            onChange={handleControlledInputChange}>
+          </textarea>
+        </div>
+      </fieldset>
+      <button type="submit"
+        onClick={evt => {
+          evt.preventDefault()
+          constructNewNews()
+        }}
+        className="btn btn-primary">
+        {editMode ? "Save those changes" : "Add that News!"}
+      </button>
+    </form>
+  )
 }
