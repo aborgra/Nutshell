e2a2c5f3931e6c7b2cc29f4ea882720aa0e228ae
wggreen <wggreen.nss@gmail.com>
2020-01-30 11:14:24 -0600

    added event.js to this branch

---
+++ src/components/events/Event.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { EventContext } from "./EventsProvider"
+import "./Events.css"
+
+
+export default ({event, props, friend, next}) => {
+
+    const { deleteEvent } = useContext(EventContext)
+
+    let eventSectionClass = "event"
+    let eventDeleteButton = ""
+    let eventEditButton = ""
+
+    if (friend === true) {
+        eventSectionClass = "friendEvent"
+    } else {
+        eventDeleteButton = <button
+                                onClick={() => {
+                                    deleteEvent(event.id)
+                                    .then(() => {
+                                        props.history.push("/")
+                                    })
+                                }}
+                                >Delete event
+                            </button>
+        eventEditButton = <button onClick={() => {
+                            props.history.push("/editEvent/:eventId(\d+)")
+                            }}
+                            >Edit event
+                          </button>
+    }
+
+    if (next === true) {
+        eventSectionClass = "latestEvent"
+    }
+
+    return (
+        <section className={eventSectionClass}>
+            <div className="eventCardInfo">Name: ${event.name}</div>
+            <div className="eventCardInfo">Date: ${event.date}</div>
+            <div className="eventCardInfo">Location: ${event.location}</div>
+            {eventEditButton}
+            {eventDeleteButton}
+        </section>
+    )
+}
