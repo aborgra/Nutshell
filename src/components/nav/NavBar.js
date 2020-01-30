2a50cc3bb78dfe3d4acd85d28443ad6701aab867
aborgra <audrey.borgra@gmail.com>
2020-01-29 18:42:57 -0600

    no major changes

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -7,7 +7,7 @@ export default props => {
     <ul className="navbar">
       <li className="navbar__item active">
         <Link className="navbar__link" to="/">
-          NSS Kennels
+          Nutshell
         </Link>
       </li>
 {
